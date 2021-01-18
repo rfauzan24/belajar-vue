8892c029aed3ae247a7dc28ddea51aadfeeb772f
Rizky Fauzan <rizky.fauzan2408@gmail.com>
2021-01-18 17:12:23 +0700

    test deploy

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="employee-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Employee Name</label>
+      <input v-model="employee.name" type="text" name="" id="">
+      <label>Employee Email</label>
+      <input v-model="employee.email" type="email" name="" id="">
+      <button>Add Employee</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'employee-form',
+  data() {
+    return {
+      employee: {
+        name: '',
+        email: '',
+      },
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log('Testing handleSubmit')
+      this.$emit('add:employee', this.employee)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+</style>
